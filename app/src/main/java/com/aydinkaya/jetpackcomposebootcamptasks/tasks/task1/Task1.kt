2de663f3cd1628c36b5bffd6863a3942c35a77de
aydinnkaya <Aydinnkaya1@gmail.com>
2024-08-06 22:22:16 +0300

    Initial commit

---
+++ app/src/main/java/com/aydinkaya/jetpackcomposebootcamptasks/tasks/task1/Task1.kt
@@ -0,0 +1,142 @@
+package com.aydinkaya.jetpackcomposebootcamptasks.tasks.task1
+
+fun main() {
+
+    val city = "Üsküdar"
+    println("City: $city")
+
+    val country = "Turkey"
+    println("Country: $country")
+
+    val phone = "+90 531 468 1288"
+    println("Phone: $phone")
+
+    val postalCode = "34660"
+    println("Postal Code: $postalCode")
+
+    val email = "aydinnkaya1@gmail.com"
+    println("Email: $email")
+
+    val profession = "Software Engineer"
+    println("Profession: $profession")
+
+    val stockQuantity = 200
+    println("Stock Quantity: $stockQuantity")
+
+    val customerName = "Aydın Kaya"
+    println("Customer Name: $customerName")
+
+    val balance = 9876.54
+    println("Balance: $balance")
+
+    val birthDate = "2000-01-02"
+    println("Birth Date: $birthDate")
+
+    val salary = 8500.0
+    println("Salary: $salary")
+
+    val maritalStatus = "Single"
+    println("Marital Status: $maritalStatus")
+
+    val productReview = "Highly recommended! Exceptional quality."
+    println("Product Review: $productReview")
+
+    val paymentDate = "2024-09-15"
+    println("Payment Date: $paymentDate")
+
+    val payment = 149.99
+    println("Payment: $payment")
+
+    val orderQuantity = 5
+    println("Order Quantity: $orderQuantity")
+
+    val carModel = "Tesla Model 3"
+    println("Car Model: $carModel")
+
+    val bookTitle = "Kotlin in Action"
+    println("Book Title: $bookTitle")
+
+    val publicationDate = "2017-11-19"
+    println("Publication Date: $publicationDate")
+
+    val discountAmount = 25.0
+    println("Discount Amount: $discountAmount")
+
+    val roomCount = 4
+    println("Room Count: $roomCount")
+
+    val latitude = 40.7128
+    println("Latitude: $latitude")
+
+    val longitude = -74.0060
+    println("Longitude: $longitude")
+
+    val productName = "Laptop"
+    println("Product Name: $productName")
+
+    val mealPrice = 29.99
+    println("Meal Price: $mealPrice")
+
+    val brand = "Samsung"
+    println("Brand: $brand")
+
+    val musicTitle = "Imagine"
+    println("Music Title: $musicTitle")
+
+    val videoDuration = 90
+    println("Video Duration: $videoDuration")
+
+    val productRating = 4.9
+    println("Product Rating: $productRating")
+
+    val imageName = "landscape.jpg"
+    println("Image Name: $imageName")
+
+    val fileFormat = "PNG"
+    println("File Format: $fileFormat")
+
+    val color = "Red"
+    println("Color: $color")
+
+    val colorCode = "#FF0000"
+    println("Color Code: $colorCode")
+
+    val phoneModel = "Samsung Galaxy S21"
+    println("Phone Model: $phoneModel")
+
+    val screenSize = 6.2
+    println("Screen Size: $screenSize")
+
+    val weight = 0.169
+    println("Weight: $weight")
+
+    val nationalDay = "Independence Day"
+    println("National Day: $nationalDay")
+
+    val holiday = "Christmas Day"
+    println("Holiday: $holiday")
+
+    val reservationDate = "2024-12-31"
+    println("Reservation Date: $reservationDate")
+
+    val streetName = "Broadway"
+    println("Street Name: $streetName")
+
+    val busRoute = "M15"
+    println("Bus Route: $busRoute")
+
+    val remainingMinutes = 10
+    println("Remaining Minutes: $remainingMinutes")
+
+    val trackingCode = "TRK987654"
+    println("Tracking Code: $trackingCode")
+
+    val couponDuration = 45
+    println("Coupon Duration: $couponDuration")
+
+    val couponCode = "DISCOUNT45"
+    println("Coupon Code: $couponCode")
+
+    val billingAddress = "Üsküdar, İstanbul, Turkey"
+    println("Billing Address: $billingAddress")
+}
